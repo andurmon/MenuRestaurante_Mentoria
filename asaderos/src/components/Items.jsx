360d4e58df32cc9ab14128c9486095a27d18630d
Herley Ocampo <lord.herley@gmail.com>
2021-03-04 23:15:32 -0500

    contadores boton

--- asaderos/src/components/Items.jsx
+++ asaderos/src/components/Items.jsx
@@ -1,57 +1,84 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import '../styles/menu.css';
-import Pollo from '../img/pollo-asado.jpg';
-import Combo1 from '../img/pollo-seervido.jpg'
-import ArrozPollo from '../img/arroz-pollo.jpg'
-import Carnes from '../img/carnes.jpg'
+import products from './products';
+import Card from './Card'
+
 
 const Items= () =>{
 
-    const [product,serProduct] = useState([
-        {
-          id: 1,
-          name: "Pollo Frito",
-          price: 19000,
-          descripcion: "Pollo mas papa platano y arepa",
-          url: Pollo,
-          counter: 0
-        },
-        {
-          id: 2,
-          name: "Combo 1",
-          price: 21000,
-          descripcion: "Pollo mas papa a la francesa platano arepa y gaseosa 1.5L",
-          url: Combo1,
-          counter: 0
-        },
-        {
-          id: 3,
-          name: "Arroz con Pollo",
-          price: 16000,
-          descripcion: "Arros amarillo con pechuga desmechada y verduras mas presa de pollo frito",
-          url: ArrozPollo,
-          counter: 0
-        },
-        {
-          id: 4,
-          name: "Tabla Mixta",
-          price: 23000,
-          descripcion: "Carne azada: res cerdo y pollo mas porcion de arroz y papa",
-          url: Carnes,
-          counter: 0
-        },
+    // const [product,serProduct] = useState([
+    //     {
+    //       id: 1,
+    //       name: "Pollo Frito",
+    //       price: 19000,
+    //       descripcion: "Pollo mas papa platano y arepa",
+    //       url: Pollo,
+    //       counter: 0
+    //     },
+    //     {
+    //       id: 2,
+    //       name: "Combo 1",
+    //       price: 21000,
+    //       descripcion: "Pollo mas papa a la francesa platano arepa y gaseosa 1.5L",
+    //       url: Combo1,
+    //       counter: 0
+    //     },
+    //     {
+    //       id: 3,
+    //       name: "Arroz con Pollo",
+    //       price: 16000,
+    //       descripcion: "Arros amarillo con pechuga desmechada y verduras mas presa de pollo frito",
+    //       url: ArrozPollo,
+    //       counter: 0
+    //     },
+    //     {
+    //       id: 4,
+    //       name: "Tabla Mixta",
+    //       price: 23000,
+    //       descripcion: "Carne azada: res cerdo y pollo mas porcion de arroz y papa",
+    //       url: Carnes,
+    //       counter: 0
+    //     },
         
-      ]);
+    //   ]);
 
+// const [number_product,setNumber_product]=useState(0);
+// function contador  (product) {
+
+//   //valor = product.price*
+//   product.counter=number_product;
+//   setNumber_product(number_product+1)
+// };
 return (
     <>
+      <div className="product-container">
+        
+        
+        {products.map((product)=>(
+        <Card 
 
-    <div className="product-container">
+        key={product.id}
+        id={product.id}
+        name={product.name}
+        price={product.price}
+        descripcion={product.descripcion}
+        url={product.url}
+        counter={product.counter}
 
-    {product.map((item)=>{
+        />
+        
+        ))}
+        
+        
+        </div>
+    {/* <div className="product-container">
+
+    {products.map((item)=>{
+
                 return (
-                    <div className="card-product">
+                    <div key={item.id} className="card-product">
+
         <div className="product-img">
             <img src={item.url} alt=""/>
         </div>
@@ -60,9 +87,15 @@ return (
             <p>{item.descripcion}</p>
             <div className="price-control"><span>${item.price}</span>
                 <div className="card-quantity">
-                    <button className="btn-quantity">-</button>
-                    <span>0</span>
-                    <button className="btn-quantity">+</button>
+                    <button className="btn-quantity" onClick={({item})=>(
+
+                      console.log("click")
+                      
+                      
+
+                    )}>-</button>
+                    <span>{number_product}</span>
+                    <button onClick= {()=>contador(item)}className="btn-quantity">+</button>
                 </div>
             </div>
         </div>
@@ -78,7 +111,7 @@ return (
 <div>
 
 <Link to="/">Bienvenidos</Link>
-</div>
+</div> */}
 
 </>
 
